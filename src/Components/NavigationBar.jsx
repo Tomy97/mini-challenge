8ec3b3ebd8be456af11002ff2551401e695ed15f
Tomy97 <64372041+Tomy97@users.noreply.github.com>
2021-05-28 09:04:57 -0300

    Agregado para el details, validaciones del formulario y para el edit

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
-import { Nav, Navbar } from 'react-bootstrap'
+import { Nav } from 'react-bootstrap'
 
 const NavigationBar = () => {
+    const a = {
+        color: '#000000',
+    }
     return (
-        <Navbar bg="light" expand="lg">
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse className="d-flex justify-content-end" id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="/" >Home</Nav.Link>
-                    <Nav.Link href="/form">Creacion De Formulario</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        <Nav className="mr-auto bg-light" >
+            <Nav.Link href="/" style={a} >Home</Nav.Link>
+            <Nav.Link href="/form" style={a} >Creacion De Formulario</Nav.Link>
+        </Nav>
+
 
     )
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,20 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { Table, Pagination } from 'react-bootstrap';
-import { Toast } from '../../alerts';
+import Swal from 'sweetalert2';
+import { Toast } from '../../alerts'
 import useFetch from '../../Hooks/useFetch';
-const Home = () => {
 
+const Home = () => {
     const { state, setStatus, handlePageChange } = useFetch();
     const [successMessage] = useState('')
     const [valid, setValid] = useState(false)
-
-    const trash = {
-        color: '#F00000'
-    }
-
-    const edit = {
-        color: '#00459C'
-    }
 
     const fireSuccesMeesage = () => {
         successMessage.length &&
@@ -33,14 +26,81 @@ const Home = () => {
             setValid(true);
             Toast.fire({
                 icon: 'success',
-                title: `La data seleccionada se elimino correctamente`
+                title: `La data seleccionada se elimino correctamente`,
+                allowOutsideClick: true,
             })
         }
     }
+    const fireFormEdit = async (editPost) => {
+
+        const { value: formValues } = await Swal.fire({
+            title: 'Editar formulario',
+            html: `
+            <label> Titulo </label>
+            <textarea id="swal-input1" class="swal2-input" cols="40" rows="20"> ${editPost.title} </textarea>
+            <label> Detalle </label>
+            <textarea id="swal-input2" class="swal2-input" cols="40" rows="20"> ${editPost.body} </textarea>
+            `,
+            focusConfirm: false,
+            showConfirmButton: true,
+            backdrop: false,
+            position: 'center',
+            confirmButtonText: 'Guardar',
+            preConfirm: () => {
+                return [
+                    document.getElementById('swal-input1').value,
+                    document.getElementById('swal-input2').value
+                ]
+            }
+        })
+        if (formValues) {
+            const prueba = JSON.stringify(formValues)
+            // const arr = new Array(prueba);
+            Toast.fire({
+                icon: 'success',
+                title: `El dato seleccionado se actualizo correctamente`,
+                allowOutsideClick: true,
+            })
+        }
+        console.log(formValues);
+
+        // const { value: formValues } = await Swal.fire({
+        //     title: 'Multiple inputs',
+        //     html:
+        //       '<input id="swal-input1" class="swal2-input">' +
+        //       '<input id="swal-input2" class="swal2-input">',
+        //     focusConfirm: false,
+        //     preConfirm: () => {
+        //       return [
+        //         document.getElementById('swal-input1').value,
+        //         document.getElementById('swal-input2').value
+        //       ]
+        //     }
+        //   })
+
+        //   if (formValues) {
+        //     Swal.fire(JSON.stringify(formValues))
+        //   }
+    }
+
+    const fireDetails = (detailsPost) => {
+        Swal.fire({
+            title: 'Detalles',
+            text: detailsPost.body,
+            allowOutsideClick: true,
+        })
+    }
+
     useEffect(fireSuccesMeesage, [successMessage])
+    const trash = {
+        color: '#F00000'
+    }
+
+    const edit = {
+        color: '#00459C'
+    }
     return (
         <>
-
             <Table responsive hover >
                 <thead>
                     <tr>
@@ -59,10 +119,13 @@ const Home = () => {
                                     {j.title}
                                 </td>
                                 <td>
-                                    <i className="far fa-edit d-flex justify-content-center" style={edit}></i>
+                                    <i className="fas fa-ellipsis-h d-flex justify-content-center" onClick={() => fireDetails(j)}></i>
                                 </td>
                                 <td>
-                                    <i edge='end' className="fas fa-trash d-flex justify-content-center" onClick={() => onRemove(j)} aria-label='delete' style={trash}></i>
+                                    <i className="far fa-edit d-flex justify-content-center" onClick={() => fireFormEdit(j)} style={edit} ></i>
+                                </td>
+                                <td>
+                                    <i edge='end' className="fas fa-trash d-flex justify-content-center" onClick={() => onRemove(j)} style={trash}></i>
                                 </td>
                             </tr>
                         ))
