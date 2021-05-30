ddccc1ec7e71c65fe44bf193006919db20c2dd9d
Tomy97 <64372041+Tomy97@users.noreply.github.com>
2021-05-30 02:15:20 -0300

    edicion, eliminado y detalle

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,15 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Table, Pagination } from "react-bootstrap";
+import { Pagination } from "react-bootstrap";
 import Swal from "sweetalert2";
 import { Toast } from "../../alerts";
+import TableComponent from "../../Components/Table";
 import useFetch from "../../Hooks/useFetch";
 
-const Home = () => {
-  const { state, setStatus, handlePageChange } = useFetch();
+const Home = (props) => {
+  const { state } = useFetch();
   const [successMessage] = useState("");
-  const [edit, setEdit] = useState({
+  const [table, setTable] = useState({
     title: "",
     body: "",
   });
@@ -20,120 +22,32 @@ const Home = () => {
       });
   };
 
-  const onRemove = (selectedPost) => {
-    const data = state.data.filter((post) => post.id !== selectedPost.id);
-    let newState = { ...state };
-    newState.data = data;
-    setStatus(newState);
-    if (newState) {
-      setValid(true);
-      Toast.fire({
-        icon: "success",
-        title: `La data seleccionada se elimino correctamente`,
-        allowOutsideClick: true,
-      });
-    }
-  };
-  const fireFormEdit = async (editPost) => {
-    const { value: formValues } = await Swal.fire({
-      title: "Editar formulario",
-      html: `
-            <label> Titulo </label>
-            <textarea id="swal-input1" class="swal2-input" cols="40" rows="20"> ${editPost.title} </textarea>
-            <label> Detalle </label>
-            <textarea id="swal-input2" class="swal2-input" cols="40" rows="20"> ${editPost.body} </textarea>
-            `,
-      focusConfirm: false,
-      showConfirmButton: true,
-      backdrop: false,
-      position: "center",
-      confirmButtonText: "Guardar",
-      preConfirm: () => {
-        return [
-          document.getElementById("swal-input1").value,
-          document.getElementById("swal-input2").value,
-        ];
-      },
-    });
-    if (formValues) {
-      const changeData = JSON.stringify(formValues);
-      Toast.fire({
-        icon: "success",
-        title: `El dato seleccionado se actualizo correctamente`,
-        allowOutsideClick: true,
-      });
-    }
-    console.log(formValues);
-  };
+  const handleChange = (e) => {
+    setTable({
+      ...table,
+      [e.target.name]: e.target.value,
+    })
+  }
 
-  const fireDetails = (detailsPost) => {
-    Swal.fire({
-      title: "Detalles",
-      text: detailsPost.body,
-      allowOutsideClick: true,
-    });
-  };
-
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios
+      .patch(state + table)
+      .then((res) => {
+        if (res.status === 200) {
+          props.edit_post_action(table)
+          Swal('Succesfully edited blog!', '', 'success')
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+        Swal('Error editing post', '', 'error')
+      })
+  }
   useEffect(fireSuccesMeesage, [successMessage]);
-  const trash = {
-    color: "#F00000",
-  };
-
-  const edit = {
-    color: "#00459C",
-  };
   return (
     <>
-      <Table responsive hover>
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Titulo</th>
-          </tr>
-        </thead>
-        <tbody>
-          {state.data.map((j) => (
-            <tr key={j.id}>
-              <td>{j.id}</td>
-              <td>{j.title}</td>
-              <td>
-                <i
-                  className="fas fa-ellipsis-h d-flex justify-content-center"
-                  onClick={() => fireDetails(j)}
-                ></i>
-              </td>
-              <td>
-                <i
-                  className="far fa-edit d-flex justify-content-center"
-                  onClick={() => fireFormEdit(j)}
-                  style={edit}
-                ></i>
-              </td>
-              <td>
-                <i
-                  edge="end"
-                  className="fas fa-trash d-flex justify-content-center"
-                  onClick={() => onRemove(j)}
-                  style={trash}
-                ></i>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-      <Pagination className="d-flex justify-content-end ">
-        {state.data.map((_, i) => {
-          return (
-            <Pagination.Item
-              onClick={() => handlePageChange(i + 1)}
-              key={i + 1}
-              active={i + 1 === state.activePage}
-            >
-              {i + 1}
-            </Pagination.Item>
-          );
-        })}
-      </Pagination>
+      <TableComponent table={table} handleChange={handleChange} handleSubmit={handleSubmit} isEdit />
     </>
   );
 };
