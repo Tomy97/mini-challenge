ab95c20ea9983fae3f209aef414c7cddcd0be1ed
Tomas Pandullo <tomas.pandu@gmail.com>
2022-12-24 19:34:59 -0300

    inicializacion del componente de crear

--- src/views/CreatePost.tsx
+++ src/views/CreatePost.tsx
@@ -1,8 +1,26 @@
+import { Card, Container, FormControl, Typography } from '@mui/material'
+import Grid from '@mui/material/Unstable_Grid2'
+import { Form } from 'formik'
 import React from 'react'
 
 const CreatePost = () => {
   return (
-    <div>createPost</div>
+    <Container sx={{ marginTop: '6rem' }} >
+      <Grid container sx={{ justifyContent: 'center' }}>
+        <Grid xs={12} md={8}>
+          <Card>
+            <Typography variant="h5" sx={{ textAlign: 'center' }} gutterBottom component="div">
+              Create a Post
+            </Typography>
+            <Grid xs={12}>
+              <FormControl component={Form}>
+                  
+              </FormControl>
+            </Grid>
+          </Card>
+        </Grid>
+      </Grid>
+    </Container>
   )
 }
 
